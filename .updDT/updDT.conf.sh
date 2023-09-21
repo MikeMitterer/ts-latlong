71465cf2e2c69b27c3eeda6683b875c0f3064b0d
Mike Mitterer <office@mikemitterer.at>
2023-09-21 13:31:50 +0200

    Version für alle Packages gleichgezogen

--- .updDT/updDT.conf.sh
+++ .updDT/updDT.conf.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 
 #------------------------------------------------------------------------------
-# Config for updateDevTemplates.sh, created on Do 14 Sep 2023 15:12:16 CEST
+# Config for updateDevTemplates.sh, created on Do 14 Sep 2023 16:01:03 CEST
 # Project-Type: ts
 #------------------------------------------------------------------------------
 
@@ -11,20 +11,13 @@
 # BASE_FOLDER="${DEV_LOCAL}/Templates/Production"
 
 # Names des Template-Folders
-TEMPLATE_FOLDER="TypeScript"
+# TEMPLATE_FOLDER="TypeScript"
 
 # Files die umbenannt werden müssen -------------------------------------------
-FILES_TO_RENAME["babel.config.js"]="babel.config.cjs"
-FILES_TO_RENAME["postcss.config.js"]="postcss.config.cjs"
-FILES_TO_RENAME["prettier.config.js"]="prettier.config.cjs"
 
-FILES_TO_RENAME["jest.config.js"]="jest.config.cjs"
-FILES_TO_RENAME["jest-puppeteer.config.js"]="jest-puppeteer.config.cjs"
+# Files die gelöscht werden ---------------------------------------------------
 
-FILES_TO_RENAME["webpack.node.js"]="webpack.node.cjs"
-FILES_TO_RENAME["webpack.ts.js"]="webpack.ts.cjs"
-FILES_TO_RENAME["webpack.web.js"]="webpack.web.cjs"
-FILES_TO_RENAME["webpack.web.local.js"]="webpack.web.local.cjs"
+# FILES_TO_REMOVE+=("jest-puppeteer.config.cjs")
 
 # Files die kopiert werden -----------------------------------------------------
 
@@ -32,37 +25,13 @@ FILES_TO_RENAME["webpack.web.local.js"]="webpack.web.local.cjs"
 # FILES_TO_COPY["static/config.lenovo.js"]="config.js"
 # FILES_TO_COPY["static/config.lenovo.js"]=""
 
-FILES_TO_COPY[".browserslistrc"]=""
-FILES_TO_COPY[".eslintrc.cjs"]=""
-FILES_TO_COPY[".npmignore"]=""
-FILES_TO_COPY[".gitignore"]=""
-
-FILES_TO_COPY["babel.config.cjs"]=""
-FILES_TO_COPY["postcss.config.cjs"]=""
-FILES_TO_COPY["prettier.config.cjs"]=""
-
-FILES_TO_COPY["jest.config.lib.cjs"]="jest.config.cjs"
-
-FILES_TO_COPY["tsconfig.json"]=""
-FILES_TO_COPY["tsconfig.lib.json"]=""
-
-FILES_TO_COPY["webpack.node.cjs"]=""
-FILES_TO_COPY["webpack.ts.cjs"]=""
-FILES_TO_COPY["webpack.web.cjs"]=""
-
 # Hängt von dem jeweiligen Projekt ab ob puppeteer benötigt wird oder nicht
-# FILES_TO_COPY["jest-puppeteer.config.js"]=""
+# FILES_TO_COPY["jest-puppeteer.config.cjs"]="false"
 
-# Einfacher Installer für einige der Standard-Packages
-# FILES_TO_COPY["setup/install.default.packages.sh"]="install.default.packages.sh"
-# FILES_TO_COPY["setup/install.puppeteer.packages.sh"]="install.puppeteer.packages.sh"
+# Scripts ---------------------------------------------------------------------
 
 # Updates ---------------------------------------------------------------------
 
-# Die Files werden nur kopiert wenn sie noch nicht existieren
-FILES_TO_UPDATE["webpack.web.local.cjs"]=""
-
-FILES_TO_UPDATE["jest.setup.js"]="tests/jest.setup.js"
 
 # Wird mit
 #     echo "${GH_ORG_MM}" > .gitorg
